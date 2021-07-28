c9b4f045efd0afe31b8ef92fba813ad451705305
WATERB(soosbk) <66404477+soosbk@users.noreply.github.com>
2021-07-28 19:33:30 +0900

    백준-1260-자동차

--- baekjoon/1260.py
+++ baekjoon/1260.py
@@ -1,68 +1,69 @@
-input_list=input().split(" ")
-n=int(input_list[0])
-m=int(input_list[1])
-v=int(input_list[2])
-li=[]
-checklist=[False for i in range(0,n)]
+import sys
+sys.setrecursionlimit(10**5)
+
+n,m,v = map(int,input().split())
+li={}
+
 for i in range(0,m):
 	input_list=input().split(" ")
-	li.append([int(input_list[0]),int(input_list[1])])
-	
-li=sorted(li,key=lambda x:(x[0],x[1]))
+	if int(input_list[0]) not in li: li[int(input_list[0])]=[int(input_list[1])]
+	else: li[int(input_list[0])].append(int(input_list[1]))
+
+	if int(input_list[1]) not in li: li[int(input_list[1])]=[int(input_list[0])]
+	else: li[int(input_list[1])].append(int(input_list[0]))
+
+for key in li:
+	li[key].sort()
+
 
 
 #DFS
+checklist=[]
 
-checklist=[False for i in range(0,n)]
-
-stack=[]
-stack.append(v)
-while True:
-	#print(queue)
-	if len(stack)==0: 
-		break
-	f=stack[-1]
-	del(stack[-1])
-	if checklist[f-1]==True: continue
-	checklist[f-1]=True
-	print(f,end=" ")
-	for i in range(m-1,-1,-1):
-
-		if (li[i][0]==f) and (checklist[li[i][1]-1]==False):
-			stack.append(li[i][1])
-			
-		elif (li[i][1]==f) and (checklist[li[i][0]-1]==False):
-			stack.append(li[i][0])
+def dfs(edge):
+	if edge not in li: return 
+	
+	k=list(li[edge])
+	for i in range(0,len(k)):
+		if str(k[i]) not in checklist: 
+			checklist.append(str(k[i]))
+			dfs(k[i])
 			
 		
-print()
+checklist.append(str(v))
+dfs(v)
+
+print(" ".join(checklist))
+	
+			
 
 #BFS
-queue=[]
-checklist=[False for i in range(0,n)]
-
+checklist2=[]
 plist=[]
-queue.append(v)
-checklist[v-1]=True
-while True:
-	plist.clear()
-	for i in range(0,len(li)):
-		if (queue[0]==li[i][1]) and (li[i][0] not in plist) and (checklist[li[i][0]-1]!=True) and (li[i][0] not in queue):
-			plist.append(li[i][0])
-
-		elif (queue[0]==li[i][0]) and (li[i][1] not in plist) and (checklist[li[i][1]-1]!=True) and (li[i][1] not in queue):
-			plist.append(li[i][1])
-
-	if len(plist)!=0: plist.sort()
-	queue=queue+plist
+def bfs(edge):
+	if edge not in li: 
+		if len(checklist2)>1: 
+			bfs(int(checklist2[1]))
+		else: return 
+	k=list(li[edge])
+	for i in range(0,len(k)):
+		if (str(k[i]) not in plist) and (str(k[i]) not in checklist2):
+			checklist2.append(str(k[i]))
+			plist.append(str(k[i]))
 	
-	checklist[queue[0]-1]=True
-	print(queue[0],end=" ")
-	del(queue[0])
-	if len(queue)==0: break
+	if len(checklist2)>0:
+		a=checklist2[0] 
+		del(checklist2[0])
+		bfs(int(a))
+
+
+	
 
 
 
-print()
-
-
+checklist2.append(str(v))
+plist.append(str(v))
+bfs(v)
+print(" ".join(plist))
+	
+	
