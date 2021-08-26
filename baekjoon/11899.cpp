694342b9409e574c407a624329a441045238dc46
WATERB(soosbk) <66404477+soosbk@users.noreply.github.com>
2021-08-26 16:56:30 +0900

    백준-11899-자전거

--- baekjoon/11899.cpp
+++ baekjoon/11899.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main(){
 	stack<char> st;
 	string input;
-
+	int count=0;
 	cin>>input;
 	for(int i=0;i<input.size();i++){
 		if(st.empty()||!((input[i]==')'&&st.top()=='('))) st.push(input[i]);
@@ -17,7 +17,7 @@ int main(){
 		
 	}
 
-	int count=0;
+	
 	while(!st.empty()) {
 		count+=1;
 		st.pop();
