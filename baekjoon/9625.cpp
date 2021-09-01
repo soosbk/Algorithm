383d4066c66f1307a336b4762dc5520008a06be2
WATERB(soosbk) <66404477+soosbk@users.noreply.github.com>
2021-09-01 17:07:44 +0900

    백준-9625-자동차

---
+++ baekjoon/9625.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+
+using namespace std;
+
+
+int main(){
+	int a_count=1;
+	int b_count=0;
+	int tmp;
+
+	int time;
+
+	cin>>time;
+
+
+	for(int i=0;i<time;i++){
+		tmp=b_count;
+		b_count=a_count+b_count;
+		a_count=tmp;
+	}
+
+
+	cout<<a_count<<" "<<b_count<<endl;
+}
